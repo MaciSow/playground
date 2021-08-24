7db7d7ecdfc3fc83e2cf2a9fb1e28704f55feca4
Maciej Sowiński <>
2021-08-24 10:41:35 +0200

    Make better describe in main page

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit files and save to reload.
         </p>
       </header>
       <Button text='Hello'/>
