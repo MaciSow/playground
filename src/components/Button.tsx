c1a0f76f802e9dd7e1e3e26fb612bc39a79786cd
Maciej Sowiński <>
2021-08-24 12:29:02 +0200

    Add ultra-wide style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,6 +5,7 @@ type ButtonProps = {
     text?: string;
 }
 
-const Button = ({text}: ButtonProps) => <button className='normal magic wide'>{text}</button>
+
+const Button = ({text}: ButtonProps) => <button className='normal magic ultra-wide'>{text}</button>
 
 export default Button;
