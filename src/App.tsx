d26539585090fb30161faa304e847a4246c6f280
Maciej Sowiński <>
2021-08-24 12:10:45 +0200

    Change text in main page

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <p>Have a nice day</p>
+                <p>Another change</p>
             </header>
             <Button text='Hello'/>
         </div>
