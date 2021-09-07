af05036751debedd1ba8aa1142ded067afb86503
Maciej Sowiński <>
2021-09-07 13:52:07 +0200

    Learn hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from "./components/Button";
+
 
 function App() {
+    const [count, setCount] = useState(0)
+
+    const myBox = useRef(null);
+    console.log(myBox);
+
+    const changeBoxColor = () => {
+        // @ts-ignore
+        myBox.current.style.background = "black";
+    }
+
+    const handleClickBox = () => {
+        setCount(count + 1)
+    }
+
+    useEffect(()=>{
+        document.title = `Box was click ${count} times`
+    },[count])
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>We need do sth with main page</p>
-            </header>
-            <Button text='Hello'/>
+            <div className="box" ref={myBox} onClick={handleClickBox}>
+                <h1 className="text">{count}</h1>
+            </div>
+            <button onClick={changeBoxColor}>Change Color</button>
         </div>
     );
 }
